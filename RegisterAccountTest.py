9d37d66760690ec98a1b3cddf788f08bc08e949a
LicensedUser <88596320+LicensedUser@users.noreply.github.com>
2026-02-13 05:17:28 +0200

    Add automated account registration test script
    Automate browser interaction for account registration testing.

---
+++ RegisterAccountTest.py
@@ -0,0 +1,32 @@
+import webbrowser
+import pyautogui
+import time
+
+# Open browser
+webbrowser.open("https://temp-mail.org/en/")
+
+# Wait 20 seconds for page to load
+time.sleep(20)
+
+# Press Ctrl + F
+pyautogui.hotkey('ctrl', 'f')
+time.sleep(1)
+
+# Type "copy"
+pyautogui.write("copy")
+time.sleep(1)
+
+# Press ESC
+pyautogui.press('esc')
+time.sleep(1)
+
+# Press TAB
+pyautogui.press('tab')
+time.sleep(1)
+
+# Press ALT + TAB
+pyautogui.hotkey('alt', 'tab')
+time.sleep(1)
+
+# Press ENTER
+pyautogui.press('enter')
