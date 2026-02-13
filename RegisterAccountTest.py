bade815dd01c5a8fe511d25a602184b039f1ff4f
LicensedUser <88596320+LicensedUser@users.noreply.github.com>
2026-02-13 05:25:30 +0200

    Update timing and add Reddit browser opening
    Adjusted sleep durations for better timing and added a new browser opening for Reddit.

--- RegisterAccountTest.py
+++ RegisterAccountTest.py
@@ -3,30 +3,33 @@ import pyautogui
 import time
 
 # Open browser
-webbrowser.open("https://temp-mail.org/en/")
+webbrowser.open("https://temp-mail.org/en")
 
 # Wait 20 seconds for page to load
 time.sleep(20)
 
 # Press Ctrl + F
 pyautogui.hotkey('ctrl', 'f')
-time.sleep(1)
+time.sleep(2)
 
 # Type "copy"
 pyautogui.write("copy")
-time.sleep(1)
+time.sleep(2)
 
 # Press ESC
 pyautogui.press('esc')
-time.sleep(1)
+time.sleep(2)
 
 # Press TAB
 pyautogui.press('tab')
-time.sleep(1)
+time.sleep(2)
 
 # Press ALT + TAB
 pyautogui.hotkey('shift', 'tab')
-time.sleep(1)
+time.sleep(2)
 
 # Press ENTER
 pyautogui.press('enter')
+
+# Open browser
+webbrowser.open("https://www.reddit.com")
