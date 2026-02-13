e38feb96bac6098725127cf1809da33119e773a4
LicensedUser <88596320+LicensedUser@users.noreply.github.com>
2026-02-13 20:29:14 +0200

    Clean up pyautogui commands in RegisterNewAccount.py
    Removed unnecessary pyautogui commands for UI interactions.

--- RegisterNewAccount.py
+++ RegisterNewAccount.py
@@ -292,54 +292,6 @@ with open(Lootpath, "a", encoding="utf-8") as file:
   
 time.sleep(2)
 
-# Press Ctrl + F
-pyautogui.hotkey('ctrl', 'f')
-time.sleep(2)
-
-# Type "Create"
-pyautogui.write("Create")
-time.sleep(2)
-
-# Press ESC
-pyautogui.press('esc')
-time.sleep(2)
-
-# Press TAB
-pyautogui.press('tab')
-time.sleep(2)
-
-# Press TAB
-pyautogui.press('tab')
-time.sleep(2)
-
-# Press ENTER
-pyautogui.press('enter')
-time.sleep(30)
-
-# Press Ctrl + F
-pyautogui.hotkey('ctrl', 'f')
-time.sleep(2)
-
-# Type "Log Out"
-pyautogui.write("Log Out")
-time.sleep(2)
-
-# Press ESC
-pyautogui.press('esc')
-time.sleep(2)
-
-# Press TAB
-pyautogui.press('tab')
-time.sleep(2)
-
-# Press ALT + TAB
-pyautogui.hotkey('shift', 'tab')
-time.sleep(2)
-
-# Press ENTER
-pyautogui.press('enter')
-time.sleep(30)
-
 # Press Ctrl + W
 pyautogui.hotkey('ctrl', 'w')
 time.sleep(2)
