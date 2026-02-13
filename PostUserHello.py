2ab2f046cbdc0373a5de114b655adccefdbc6359
LicensedUser <88596320+LicensedUser@users.noreply.github.com>
2026-02-13 20:13:38 +0200

    Add automated post writing to PostUserHello.py

--- PostUserHello.py
+++ PostUserHello.py
@@ -9,3 +9,8 @@ webbrowser.open("https://chatgpt.com")
 
 # Wait 60 seconds for page to load
 time.sleep(60)
+
+pyautogui.write('Write 1 hello post from new reddit user in csv format, ask users to engage upvote comment ,50-70 words > title,post')
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(60)
