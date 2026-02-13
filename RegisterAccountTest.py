d888b7c0f14653ec783aeb3c028bf73d26035d40
LicensedUser <88596320+LicensedUser@users.noreply.github.com>
2026-02-13 05:29:19 +0200

    Fix typos in automated UI test script

--- RegisterAccountTest.py
+++ RegisterAccountTest.py
@@ -13,7 +13,7 @@ pyautogui.hotkey('ctrl', 'f')
 time.sleep(2)
 
 # Type "copy"
-pyautogui.write("copy")
+pyautogui.write("Copy")
 time.sleep(2)
 
 # Press ESC
@@ -30,6 +30,50 @@ time.sleep(2)
 
 # Press ENTER
 pyautogui.press('enter')
+time.sleep(2)
 
 # Open browser
 webbrowser.open("https://www.reddit.com")
+
+# Wait 20 seconds for page to load
+time.sleep(20)
+
+# Press Ctrl + F
+pyautogui.hotkey('ctrl', 'f')
+time.sleep(2)
+
+# Type "copy"
+pyautogui.write("Log In")
+time.sleep(2)
+
+# Press ESC
+pyautogui.press('esc')
+time.sleep(2)
+
+# Press ENTER
+pyautogui.press('enter')
+time.sleep(2)
+
+# Press Ctrl + F
+pyautogui.hotkey('ctrl', 'f')
+time.sleep(2)
+
+# Type "copy"
+pyautogui.write("Sigh Up")
+time.sleep(2)
+
+# Press ESC
+pyautogui.press('esc')
+time.sleep(2)
+
+# Press TAB
+pyautogui.press('tab')
+time.sleep(2)
+
+# Press ALT + TAB
+pyautogui.hotkey('shift', 'tab')
+time.sleep(2)
+
+# Press ENTER
+pyautogui.press('enter')
+time.sleep(2)
