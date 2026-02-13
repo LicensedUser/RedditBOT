a6a0e2c5da5c1b3694eb37918d3ae5b42ad16a36
LicensedUser <88596320+LicensedUser@users.noreply.github.com>
2026-02-13 19:23:25 +0200

    Add script to delete browser history
    Automate the process of deleting browser history using pyautogui.

---
+++ DeleteBrowserHistory.py
@@ -0,0 +1,38 @@
+import webbrowser
+import pyautogui
+import time
+
+webbrowser.open("https://www.hey.lt")
+time.sleep(10)
+
+# Press ALT + F
+pyautogui.hotkey('alt', 'f')
+time.sleep(2)
+
+for _ in range(4):
+    pyautogui.press('right')
+    time.sleep(1)
+
+pyautogui.press('down')
+time.sleep(2)
+pyautogui.press('enter')
+
+for _ in range(5):
+    pyautogui.press('tab')
+    time.sleep(1)
+
+pyautogui.press('space')
+time.sleep(2)
+
+pyautogui.press('tab')
+time.sleep(2)
+
+pyautogui.press('tab')
+time.sleep(2)
+
+pyautogui.press('enter')
+time.sleep(2)
+
+# Press CTRL + W
+pyautogui.hotkey('ctrl', 'w')
+time.sleep(2)
